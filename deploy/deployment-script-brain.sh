8971acdc238c452a6ac3bf2d63439fb7f66baec9
Mauricio S Otta <mauricio@portnumber53.com>
2024-02-19 22:12:25 -0800

    deploy update

--- deploy/deployment-script-brain.sh
+++ deploy/deployment-script-brain.sh
@@ -5,3 +5,21 @@ set -e
 
 hostname
 pwd
+
+
+# Capture parameters into variables
+DEPLOY_BASE_PATH=$1
+DEPLOYMENT_RELEASE_PATH=$2
+DEPLOYMENT_PATH=$3
+TIMESTAMP=$4
+
+
+echo "copying systemd files"
+sudo rsync -ravp --progress ./deploy/brain/systemd/ /etc/systemd/system/
+sudo systemctl daemon-reload
+
+
+
+cd $DEPLOY_BASE_PATH
+
+sudo ln -sfn $DEPLOYMENT_RELEASE_PATH$TIMESTAMP current
