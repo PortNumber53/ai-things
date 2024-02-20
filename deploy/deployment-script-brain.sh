93b7c97ba79473b87efbca85683fb55b1b0a63ed
Mauricio S Otta <mauricio@portnumber53.com>
2024-02-20 00:15:04 -0800

    switching to laravel worker

--- deploy/deployment-script-brain.sh
+++ deploy/deployment-script-brain.sh
@@ -14,12 +14,18 @@ DEPLOYMENT_PATH=$3
 TIMESTAMP=$4
 
 
-echo "copying systemd files"
+echo "-Preparing systemd files"
 cd /etc/systemd/system/
+
+sudo ln -sfn /deploy/ai-things/current/deploy/brain/systemd/laravel-worker@.service laravel-worker@text-fun-facts.service
 sudo ln -sfn /deploy/ai-things/current/deploy/brain/systemd/ai_generate_fun_facts.service
 sudo systemctl daemon-reload
 
+# Disable servies
+sudo systemctl disable --now ai_generate_fun_facts.service
 
+# Enable services
+sudo systemctl enable --now laravel-worker@text-fun-facts.service
 
 cd $DEPLOY_BASE_PATH
 
