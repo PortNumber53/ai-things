e83d1b761e75fc0e58f350008ab3054fd4527a8b
Mauricio S Otta <mauricio@portnumber53.com>
2024-02-22 08:07:06 -0800

    symlink for legion

--- deploy/deployment-script-legion.sh
+++ deploy/deployment-script-legion.sh
@@ -5,3 +5,16 @@ set -e
 
 hostname
 pwd
+
+# Capture parameters into variables
+DEPLOY_BASE_PATH=$1
+DEPLOYMENT_RELEASE_PATH=$2
+DEPLOYMENT_PATH=$3
+TIMESTAMP=$4
+
+echo "Updating release symlink"
+cd ${DEPLOYMENT_PATH}/api
+ln -sfn ${DEPLOY_BASE_PATH}storage storage
+
+cd ${DEPLOY_BASE_PATH}
+ln -sfn ${DEPLOYMENT_PATH} ./current
