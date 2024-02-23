9520c4a626e817ba68e89037c721dbb2755027fe
Mauricio S Otta <mauricio@portnumber53.com>
2024-02-23 01:26:20 -0800

    Added split jobs

--- api/app/Console/Commands/TTSSplitJobs.php
+++ api/app/Console/Commands/TTSSplitJobs.php
@@ -26,9 +26,48 @@ class TTSSplitJobs extends Command
      */
     public function handle()
     {
+        $jobTemplate = [
+            'text' => 'Sample text,',
+            'voice' => 'pat',
+            'filename' => 'sample_file_rando_voice',
+        ];
         $content = Content::where('status', 'new')->first();
-
-
         dump($content);
+
+
+
+        $queueManager = app('queue');
+        $queue = $queueManager->connection('rabbitmq');
+        if (!empty($content['title'])) {
+            // create job for title
+        }
+        if (!empty($content['sentences'])) {
+            // create job per sentence
+            foreach (json_decode($content['sentences'], true) as $index => $sentence_data) {
+
+
+                $text = $sentence_data['content'];
+                if ($text !== '<spacer>') {
+                    $jsonPayload = $jobTemplate;
+                    $jsonPayload['text'] = $text;
+                    $jsonPayload['filename'] = str_pad($content['id'], 10, '0', STR_PAD_LEFT) . '-' .
+                        str_pad($index, 3, '0', STR_PAD_LEFT) . '-' . md5($text);
+
+
+                    $jsonPayload = json_encode($jsonPayload);
+                    $queue->pushRaw($jsonPayload, 'tts_wave');
+                }
+
+
+                dump($sentence_data['content']);
+            }
+        }
+
+
+
+        // dump($jsonPayload);
+
+        // $queue->pushRaw($jsonPayload, 'testing_queue');
+        // dump($content);
     }
 }
