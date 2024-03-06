0ba77308ba074220f449af5df224b00608a117b9
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-05 19:01:05 -0800

    Improved command that checks wavs are processed

--- api/app/Console/Commands/CheckSentencesMatchCommand.php
+++ api/app/Console/Commands/CheckSentencesMatchCommand.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands;
 
+use AllowDynamicProperties;
 use Illuminate\Console\Command;
 use App\Models\Content;
+use Illuminate\Support\Facades\Artisan;
 
 class CheckSentencesMatchCommand extends Command
 {
@@ -29,28 +31,56 @@ class CheckSentencesMatchCommand extends Command
         $modifiedCount = 0;
 
         foreach ($records as $record) {
-            $sentences = collect(json_decode($record->sentences, true))->where('content', '!=', '<spacer>');
+            $sentences = collect(json_decode($record->sentences, true))->reject(function ($sentence) {
+                return strpos($sentence['content'], '<spacer') === 0;
+            });
 
             $meta = optional(json_decode($record->meta));
-            $filenames = collect($meta->filenames ?? []);
+            $filenames = collect($meta->filenames ?? [])->toArray(); // Convert to array
 
-            if ($sentences->count() === $filenames->count()) {
-                $allMatch = true;
-                foreach ($sentences as $sentence) {
-                    $sentenceId = $sentence['count'];
-                    $match = $filenames->contains('sentence_id', $sentenceId);
-                    if (!$match) {
-                        $allMatch = false;
-                        break;
-                    }
-                }
+            // Check for title filename
+            $titleFilename = sprintf('%010d-%03d-', $record->id, 0);
+            $titleFilenameExists = false;
 
-                if ($allMatch && $record->type !== 'gemini.wav_ready') {
-                    $record->type = 'gemini.wav_ready';
-                    $record->save();
-                    $modifiedCount++;
+            $allMatch = true;
+            foreach ($sentences as $sentence) {
+                $sentenceId = $sentence['count'];
+                $match = collect($filenames)->contains('sentence_id', $sentenceId);
+                if (!$match) {
+                    $this->info("Record with ID $record->id sentence $sentenceId has no filename.");
+                    $allMatch = false;
+                    break;
                 }
             }
+
+            // Check for title filename
+            foreach ($filenames as $filenameData) {
+                if (strpos($filenameData->filename, $titleFilename) !== false) { // Accessing as object
+                    // Partial match found for title filename
+                    $titleFilenameExists = true;
+                    break;
+                }
+            }
+
+            if (!$titleFilenameExists) {
+                $this->info("Record with ID $record->id has no title filename.");
+                $allMatch = false;
+            }
+
+            $allMatchStr = ($allMatch === true) ? 'all match' : 'not all match';
+            $this->line("Record with ID $record->id and $allMatchStr");
+            if ($allMatch && $record->type !== 'gemini.wav_ready') {
+                $record->type = 'gemini.wav_ready';
+                $record->save();
+                $modifiedCount++;
+            }
+        }
+
+
+
+        // Call the tts:SplitJobs command after records are updated
+        if ($modifiedCount > 0) {
+            Artisan::call('tts:SplitJobs');
         }
 
         if ($id) {
