617e2fb298a398272b6185939d1e2b568ff1fcc9
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-09 14:00:10 -0800

    Improving combine_wav script

--- tortoise-tts/tortoise/combine_wave.py
+++ tortoise-tts/tortoise/combine_wave.py
@@ -1,35 +1,22 @@
-import psycopg2
 import json
 import os
 import numpy as np
 import soundfile as sf
-from dotenv import load_dotenv
 import argparse
 import wave
 import re
+from my_database import get_database_connection
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Get the BASE_OUTPUT_FOLDER from the environment variables
 BASE_OUTPUT_FOLDER = os.getenv('BASE_OUTPUT_FOLDER', '/output/')
 
 def get_wav_and_spacer_paths_from_database(content_id):
     try:
-        connection = psycopg2.connect(
-            host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT'),
-            database=os.getenv('DB_DATABASE'),
-            user=os.getenv('DB_USERNAME'),
-            password=os.getenv('DB_PASSWORD')
-        )
+        connection = get_database_connection()
         cursor = connection.cursor()
 
-        # Fetch filenames from meta field
         cursor.execute("SELECT meta FROM contents WHERE id=%s", (content_id,))
         meta_result = cursor.fetchone()
 
-        # Fetch sentences from sentences column
         cursor.execute("SELECT sentences FROM contents WHERE id=%s", (content_id,))
         sentences_result = cursor.fetchone()
 
@@ -43,32 +30,24 @@ def get_wav_and_spacer_paths_from_database(content_id):
             if 'filenames' in meta:
                 filenames_json = meta['filenames']
 
-                # Process spacer contents
                 spacers = []
                 for entry in sentences:
                     if entry['content'].startswith("<spacer"):
                         match = re.search(r"<spacer\s*(\d*)>", entry['content'])
-                        if match:
-                            spacer_length = int(match.group(1)) if match.group(1) else 1
-                        else:
-                            spacer_length = 1
+                        spacer_length = int(match.group(1)) if match else 1
                         spacers.append({"content": entry['content'], "count": entry['count'], "length": spacer_length})
 
-                # Process WAV filenames
                 if isinstance(filenames_json, list):
                     filenames = []
                     for entry in sorted(filenames_json, key=lambda x: x.get('sentence_id', 0)):
                         if isinstance(entry, dict):
                             filename = entry.get('filename')
-                            filename = os.path.join(BASE_OUTPUT_FOLDER, 'waves', filename)  # Construct filename with BASE_OUTPUT_FOLDER
+                            filename = os.path.join(BASE_OUTPUT_FOLDER, 'waves', filename)
                             if filename:
                                 filenames.append(filename)
                         else:
                             print("Invalid entry in filenames_json:", entry)
-                    # Merge spacer contents and filenames
                     combined_list = spacers + [{"filename": filename, "count": count+len(spacers), "length": 0} for count, filename in enumerate(filenames, start=1)]
-
-                    # Sort combined list based on count
                     combined_list.sort(key=lambda x: x['count'])
 
                     return combined_list
@@ -88,21 +67,16 @@ def get_wav_and_spacer_paths_from_database(content_id):
         import traceback
         traceback.print_exc()
         print("An unexpected error occurred:", e)
-        return []
 
 
 def combine_wav_files_with_silence(wav_paths, output_path, silence_duration=1):
     combined_frames = []
     sample_rate = None
-    total_samples = 0  # Variable to store the total number of samples
+    total_samples = 0
 
-    # First pass: Find the first filename entry to determine the sample rate
-    # Sample entry: {'filename': '/output/waves/0000000006-046-tom-40f171766d6cbc52ee9cf4d86f440767.wav', 'count': 83, 'length': 0}
     for wav_path in wav_paths:
-        # print(wav_path)
         if isinstance(wav_path, dict) and 'filename' in wav_path:
             frames, sample_rate = sf.read(wav_path['filename'], dtype='float32')
-            print("FOUND IT!")
             break
 
     if sample_rate is None:
@@ -111,7 +85,6 @@ def combine_wav_files_with_silence(wav_paths, output_path, silence_duration=1):
     silence_samples = int(silence_duration * sample_rate)
     combined_frames.append(np.zeros((int(silence_samples / 2), 1), dtype=np.float32))
 
-    # Second pass: Combine the WAV files and silence from spacers
     for wav_path in wav_paths:
         if isinstance(wav_path, dict) and 'filename' in wav_path:
             filename = wav_path['filename']
@@ -129,20 +102,17 @@ def combine_wav_files_with_silence(wav_paths, output_path, silence_duration=1):
                 print(f'File {filename} does not exist.')
 
     combined_frames = np.concatenate(combined_frames, axis=0)
-
-    # Write the combined frames to the output file
     sf.write(output_path, combined_frames, sample_rate)
+    duration = get_wav_duration(output_path)
+
+    return output_path, duration
 
 
 def get_wav_duration(file_path):
     with wave.open(file_path, 'rb') as wav_file:
-        # Get the number of frames and the frame rate
         num_frames = wav_file.getnframes()
         frame_rate = wav_file.getframerate()
-
-        # Calculate the duration in seconds
         duration = num_frames / float(frame_rate)
-
         return duration
 
 if __name__ == "__main__":
@@ -151,20 +121,27 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     wav_paths = get_wav_and_spacer_paths_from_database(args.content_id)
-    # print(wav_paths)
-
     if not wav_paths:
         print("No filenames retrieved for the specified content ID.")
     else:
-        # Construct the output path using BASE_OUTPUT_FOLDER and content_id
         output_path = os.path.join(BASE_OUTPUT_FOLDER, 'waves', f'output_combined_with_silence_{args.content_id}.wav')
         try:
-            combine_wav_files_with_silence(wav_paths, output_path, silence_duration=1)
-
-            # Get duration of the combined WAV file
-            duration = get_wav_duration(output_path)
-
+            output_path, duration = combine_wav_files_with_silence(wav_paths, output_path, silence_duration=1)
             print(f"Combined WAV files saved to: {output_path}")
             print(f"Duration of the combined WAV file is: {duration:.2f} seconds")
+
+            try:
+                connection = get_database_connection()
+                cursor = connection.cursor()
+                cursor.execute("UPDATE contents SET meta = jsonb_set(meta, '{combined_wav}', %s) WHERE id=%s",
+                               (json.dumps({"filename": output_path, "duration": duration}), args.content_id))
+                connection.commit()
+                cursor.close()
+                connection.close()
+                print("Meta field updated successfully.")
+            except psycopg2.Error as e:
+                print("Error connecting to the database:", e)
+            except Exception as e:
+                print("An unexpected error occurred while updating the meta field:", e)
         except ValueError as e:
             print("Error:", e)
