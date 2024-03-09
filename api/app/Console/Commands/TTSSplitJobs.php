9eea2562455f925f121a5cfd8af03cb3b1fe11bb
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-09 15:48:17 -0800

    Allowed reprocessing of sentence

--- api/app/Console/Commands/TTSSplitJobs.php
+++ api/app/Console/Commands/TTSSplitJobs.php
@@ -4,85 +4,108 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Content;
+use Illuminate\Contracts\Queue\Queue;
+use Illuminate\Queue\SerializableClosure;
 
 class TTSSplitJobs extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'tts:SplitJobs {content_id?}';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'tts:SplitJobs {content_id?} {sentence_id?}';
     protected $description = 'Queue job for TTS';
 
-    /**
-     * Execute the console command.
-     */
+    protected $queue;
+
+    public function __construct(Queue $queue)
+    {
+        parent::__construct();
+        $this->queue = $queue;
+    }
+
     public function handle()
     {
-        $jobTemplate = [
-            'text' => 'Sample text,',
-            'voice' => 'tom',
-            'filename' => 'sample_file_random_voice',
-            'content_id' => 0,
-        ];
-
         $contentId = $this->argument('content_id');
+        $sentenceId = $this->argument('sentence_id');
 
-        if ($contentId) {
-            $content = Content::find($contentId);
-        } else {
-            $content = Content::where('status', 'new')->first();
+        if (!is_numeric($contentId)) {
+            $this->error('Invalid content ID.');
+            return;
         }
+
+        $content = Content::find($contentId);
 
         if (!$content) {
             $this->error('No content found.');
             return;
         }
 
+        if ($sentenceId !== null) {
+            $this->allowReprocessingOfSentence($content, $sentenceId);
+        }
+
+        $this->processContent($content);
+    }
+
+
+    private function allowReprocessingOfSentence(Content $content, $sentenceId)
+    {
+        // Remove filename entry associated with the sentence ID
+        $meta = json_decode($content->meta, true);
+        $meta['filenames'] = array_values(array_filter($meta['filenames'], function ($filename) use ($sentenceId) {
+            return $filename['sentence_id'] != $sentenceId;
+        }));
+        $content->meta = json_encode($meta);
+        $content->save();
+
+        // Reprocess the specific sentence here
+        $this->info("Sentence $sentenceId reprocessed.");
+    }
+
+    private function processContent(Content $content)
+    {
         $voice = 'pat';
 
-        $queueManager = app('queue');
-        $queue = $queueManager->connection('rabbitmq');
         if (!empty($content['title'])) {
-            $title = $content['title'];
-            $jsonPayload = $jobTemplate;
-            $jsonPayload['content_id'] = $content['id'];
-            $jsonPayload['text'] = $title;
-            $jsonPayload['sentence_id'] = 0;
-            $jsonPayload['filename'] = str_pad($content['id'], 10, '0', STR_PAD_LEFT) . '-' .
-                str_pad(0, 3, '0', STR_PAD_LEFT) . '-' . $voice . '-' . md5($title) . '.wav';
-
-            $jsonPayload = json_encode($jsonPayload);
-            $queue->pushRaw($jsonPayload, 'tts_wave');
-            $this->line("TITLE : " . $title);
+            $this->createJob($content, $content['title'], 0);
         }
+
         if (!empty($content['sentences'])) {
-            // create job per sentence
             foreach (json_decode($content['sentences'], true) as $index => $sentence_data) {
                 $text = $sentence_data['content'];
-                if (strpos($text, '<spacer ') !== 0) {
-                    // Process if the text doesn't start with "<spacer "
-                    $jsonPayload = $jobTemplate;
-                    $jsonPayload['content_id'] = $content['id'];
-                    $jsonPayload['sentence_id'] = $index; // Add the index as 'sentence_id'
-                    $jsonPayload['text'] = $text;
-                    $jsonPayload['filename'] = str_pad($content['id'], 10, '0', STR_PAD_LEFT) . '-' .
-                        str_pad($index, 3, '0', STR_PAD_LEFT) . '-' . $voice . '-' . md5($text) . '.wav';
 
-                    $jsonPayload = json_encode($jsonPayload);
-                    dump($jsonPayload);
-                    $queue->pushRaw($jsonPayload, 'tts_wave');
-                    $indexStr = str_pad($index, 10, ' ', STR_PAD_LEFT);
-                    $this->line("$indexStr : " . $sentence_data['content']);
+                if (strpos($text, '<spacer ') !== 0) {
+                    $this->createJob($content, $text, $index);
                 }
             }
         }
+    }
+
+    private function createJob(Content $content, $text, $index)
+    {
+        $jobPayload = $this->generateJobPayload($content, $text, $index);
+        $this->queue->pushRaw($jobPayload, 'tts_wave');
+        $this->line("$index : " . $text);
+    }
+
+    private function generateJobPayload(Content $content, $text, $index)
+    {
+        $jobTemplate = [
+            'text' => $text,
+            'voice' => 'pat',
+            'filename' => $this->generateFilename($content, $text, $index),
+            'content_id' => $content->id,
+            'sentence_id' => $index
+        ];
+
+        return json_encode($jobTemplate);
+    }
+
+    private function generateFilename(Content $content, $text, $index)
+    {
+        return sprintf(
+            "%010d-%03d-%s-%s.wav",
+            $content->id,
+            $index,
+            'pat',
+            md5($text)
+        );
     }
 }
