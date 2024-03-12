5d56caaf67dd50f50746884f3d4e74e6ad4d8294
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-11 20:56:54 -0700

    optmized audio:ConvertToMp3 command

--- api/app/Console/Commands/AudioConvertToMp3.php
+++ api/app/Console/Commands/AudioConvertToMp3.php
@@ -4,17 +4,19 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Content;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 
 class AudioConvertToMp3 extends Command
 {
-    protected $signature = 'audio:ConvertToMp3 {contentId : The content ID}';
+    protected $signature = 'audio:ConvertToMp3 {content_id? : The content ID}';
     protected $description = 'Convert audio file(s) to mp3 using ffmpeg';
 
     public function handle()
     {
-        $contentId = $this->argument('contentId');
-
-        $content = Content::find($contentId);
+        $content_id = $this->argument('content_id');
+        $content = $content_id ? Content::find($content_id) : Content::where('status', 'wav.generated')
+            ->where('type', 'gemini.payload')->first();
 
         if (!$content) {
             $this->error('Content not found.');
@@ -30,37 +32,33 @@ class AudioConvertToMp3 extends Command
             $inputFile = $filenameData['filename'];
             $sentenceId = $filenameData['sentence_id'] ?? null;
 
-            // Append base folder to input file path
-            $inputFileWithPath = env('OUTPUT_FOLDER') . "/waves/$inputFile";
+            $inputFileWithPath = config('app.output_folder') . "/waves/$inputFile";
 
-            if (!file_exists($inputFileWithPath)) {
+            if (!File::exists($inputFileWithPath)) {
                 $this->error("Input file does not exist: $inputFileWithPath");
-                die("stop\n");
+                continue;
             }
 
-            $outputFile = env('OUTPUT_FOLDER') . '/waves/' . pathinfo($inputFile, PATHINFO_FILENAME) . '.mp3';
+            $outputFile = pathinfo($inputFile, PATHINFO_FILENAME) . '.mp3';
+            $outputFullPath = config('app.output_folder') . "/waves/$outputFile";
 
-            $command = "ffmpeg -y -i $inputFileWithPath -acodec libmp3lame $outputFile";
+            $command = "ffmpeg -y -i $inputFileWithPath -acodec libmp3lame $outputFullPath";
             exec($command, $output, $returnCode);
 
-            if ($returnCode === 0) {
-                $this->info("Audio file converted successfully: $outputFile");
-                if (file_exists($outputFile) && time() - filemtime($outputFile) < 60) {
-                    $this->info("Audio MP3 file created properly.");
-                    $convertedFiles[$key] = ['filename' => $outputFile, 'sentence_id' => $sentenceId];
-                } else {
-                    $this->error("Failed to create audio MP3 file properly.");
-                }
+            if ($returnCode === 0 && File::exists($outputFullPath) && time() - File::lastModified($outputFullPath) < 60) {
+                $this->info("Audio file converted successfully: $outputFullPath");
+                $this->info("Audio MP3 file created properly.");
+                $convertedFiles[$key] = ['filename' => $outputFile, 'sentence_id' => $sentenceId];
             } else {
-                $this->error("Failed to convert audio file.");
+                $this->error("Failed to convert or create audio MP3 file.");
             }
         }
 
-        // Update content status and filepaths after all files have been successfully processed
         if (!empty($convertedFiles)) {
             $content->status = 'mp3.generated';
             $meta['filenames'] = $convertedFiles;
             $content->meta = json_encode($meta);
+
             $content->save();
         }
     }
