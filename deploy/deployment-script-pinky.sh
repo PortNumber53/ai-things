2f857df2fad13d7ce44af4aea3dac40dbd3e000e
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-13 13:02:16 -0700

    added generate_wav to pinky

--- deploy/deployment-script-pinky.sh
+++ deploy/deployment-script-pinky.sh
@@ -25,6 +25,7 @@ cd /etc/systemd/system/
 
 sudo ln -sfn /deploy/ai-things/current/deploy/pinky/systemd/convert_to_mp3.service convert_to_mp3.service
 sudo ln -sfn /deploy/ai-things/current/deploy/pinky/systemd/gemini_generate_fun_facts.service gemini_generate_fun_facts.service
+sudo ln -sfn /deploy/ai-things/current/deploy/brain/systemd/generate_wav.service
 sudo systemctl daemon-reload
 
 # Run migrations
@@ -40,3 +41,4 @@ sudo systemctl enable --now convert_to_mp3.service
 # Restart services
 sudo systemctl restart gemini_generate_fun_facts.service
 sudo systemctl restart convert_to_mp3.service
+sudo systemctl start generate_wav.service
