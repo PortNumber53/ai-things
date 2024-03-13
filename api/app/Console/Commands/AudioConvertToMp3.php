80007b28ebab6fa95ce4524709337183a4ad3ba7
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-13 00:28:28 -0700

    Added queue processing for convert to mp3 job

--- api/app/Console/Commands/AudioConvertToMp3.php
+++ api/app/Console/Commands/AudioConvertToMp3.php
@@ -4,17 +4,49 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Content;
+use Illuminate\Contracts\Queue\Queue;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 
 class AudioConvertToMp3 extends Command
 {
+    protected $queue;
+
     protected $signature = 'audio:ConvertToMp3 {content_id? : The content ID}';
     protected $description = 'Convert audio file(s) to mp3 using ffmpeg';
+    protected $content;
+
+    public function __construct(Content $content, Queue $queue)
+    {
+        parent::__construct();
+        $this->content = $content;
+        $this->queue = $queue;
+    }
 
     public function handle()
     {
         $content_id = $this->argument('content_id');
+        if (!$content_id) {
+            $this->processQueueMessage();
+        }
+        $this->processContent($content_id);
+    }
+
+    private function processQueueMessage()
+    {
+        while (true) {
+            $message = $this->queue->pop('generate_mp3');
+            $payload = json_decode($message->getRawBody(), true);
+
+            if (isset($payload['content_id'])) {
+                $this->processContent($payload['content_id']);
+                $message->delete();
+            }
+        }
+    }
+
+    private function processContent($content_id)
+    {
         $content = $content_id ? Content::find($content_id) : Content::where('status', 'wav.generated')
             ->where('type', 'gemini.payload')->first();
 
@@ -49,6 +81,8 @@ class AudioConvertToMp3 extends Command
                 $this->info("Audio file converted successfully: $outputFullPath");
                 $this->info("Audio MP3 file created properly.");
                 $convertedFiles[$key] = ['filename' => $outputFile, 'sentence_id' => $sentenceId];
+                $this->line("Removed $inputFileWithPath");
+                unlink($inputFileWithPath);
             } else {
                 $this->error("Failed to convert or create audio MP3 file.");
             }
