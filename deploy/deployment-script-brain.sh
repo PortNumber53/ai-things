616bfa72bdb48116649bf65cfd67b48b8109d3cd
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-13 11:55:27 -0700

    Need a better workflow to deploy new services

--- deploy/deployment-script-brain.sh
+++ deploy/deployment-script-brain.sh
@@ -39,7 +39,7 @@ composer install --no-ansi
 
 # Enable services
 sudo systemctl disable --now laravel-worker@text_fun_facts.service
-sudo systemctl disable --now generate_wav.service
+# sudo systemctl disable --now generate_wav.service
 
 # Restart services
 sudo systemctl stop laravel-worker@text_fun_facts.service
