29de8c1bbdd17c998136642090ca9ef0c47f654c
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-19 23:32:37 -0700

    Added generate Image job

--- manager/app/Console/Commands/JobGenerateImage.php
+++ manager/app/Console/Commands/JobGenerateImage.php
@@ -7,6 +7,8 @@ use App\Models\Content;
 use Illuminate\Contracts\Queue\Queue;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
 
 class JobGenerateImage extends BaseJobCommand
 {
@@ -33,14 +35,28 @@ class JobGenerateImage extends BaseJobCommand
         }
 
         try {
-            $meta = json_decode($this->content->meta, true);
-            $filenames = $meta['filenames'] ?? [];
-            $subtitles = $meta['subtitles'] ?? [];
-            $images = $meta['images'] ?? [];
+            // Fetch placeholder image from picsum.photos
+            $imageData = Http::get('https://picsum.photos/1280/800')->body();
 
+            // Generate filename
+            $filename = sprintf("%010d.jpg", $this->content->id);
+
+            // Define output path
+            $outputPath = config('app.output_folder') . "/images/$filename";
+
+            // Save image to output folder
+            file_put_contents($outputPath, $imageData);
+
+            // Update meta.images data point
+            $meta = json_decode($this->content->meta, true);
+            $meta['images'][] = $filename;
+            $this->content->meta = json_encode($meta);
 
             $this->content->status = $this->queue_output;
             $this->content->save();
+        } catch (\Exception $e) {
+            $this->error('Error occurred: ' . $e->getMessage());
+            return 1;
         } finally {
             $job_payload = json_encode([
                 'content_id' => $this->content->id,
