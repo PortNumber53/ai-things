9726b1e09aa4516af0fda4bbcd32dddc33ba6dbd
Mauricio S Otta <mauricio@portnumber53.com>
2024-03-23 11:16:29 -0700

    Add range to content:query command

--- manager/app/Console/Commands/ContentQueryCommand.php
+++ manager/app/Console/Commands/ContentQueryCommand.php
@@ -10,7 +10,7 @@ class ContentQueryCommand extends Command
 {
     protected $queue;
 
-    protected $signature = 'content:query';
+    protected $signature = 'content:query {start?} {end?}';
     protected $description = 'Query the contents table where status is new or gemini.payload';
 
     public function __construct(Queue $queue)
@@ -24,19 +24,27 @@ class ContentQueryCommand extends Command
         // Output the initial message
         $this->info("Fetching contents with status 'new' and type 'gemini.payload'...");
 
-        // Fetch contents in chunks of 100 records
-        Content::where('status', 'new')
-            ->where('type', 'gemini.payload')
-            ->chunk(100, function ($contents) {
-                foreach ($contents as $content) {
-                    $this->line("ID: {$content->id}, Status: {$content->status}, Type: {$content->type}");
+        $start = $this->argument('start');
+        $end = $this->argument('end');
 
-                    $job_payload = json_encode([
-                        'content_id' => $content->id,
-                    ]);
-                    $this->queue->pushRaw($job_payload, 'generate_wav');
-                }
-            });
+        $query = Content::where('status', 'new')
+            ->where('type', 'gemini.payload');
+
+        if ($start !== null && $end !== null) {
+            $query->whereBetween('id', [$start, $end]);
+        }
+
+        // Fetch contents in chunks of 10 records
+        $query->chunk(10, function ($contents) {
+            foreach ($contents as $content) {
+                $this->line("ID: {$content->id}, Status: {$content->status}, Type: {$content->type}");
+
+                $job_payload = json_encode([
+                    'content_id' => $content->id,
+                ]);
+                $this->queue->pushRaw($job_payload, 'generate_wav');
+            }
+        });
 
         // Output the completion message
         $this->info("Query completed.");
