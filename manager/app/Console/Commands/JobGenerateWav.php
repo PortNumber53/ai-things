8e63203d6ddbb36cf46bd0cae0da28e4914ee35d
Mauricio S Otta <mauricio@portnumber53.com>
2024-04-03 00:53:40 -0700

    typo

--- manager/app/Console/Commands/JobGenerateWav.php
+++ manager/app/Console/Commands/JobGenerateWav.php
@@ -89,10 +89,6 @@ class JobGenerateWav extends BaseJobCommand
             }
         }
 
-
-        die("\n\n");
-
-
         $this->content = Content::where('id', $content_id)->first();
         if (empty($this->content)) {
             $this->error("Content not found.");
