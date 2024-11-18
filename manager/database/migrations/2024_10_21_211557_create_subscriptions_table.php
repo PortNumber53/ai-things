9f35a9fcb888fd8e580fa048855d3963a610c264
Mauricio S Otta <mauricio@portnumber53.com>
2024-11-18 01:51:24 -0800

    Added subscription files

---
+++ manager/database/migrations/2024_10_21_211557_create_subscriptions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('feed_url')->unique();
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('site_url')->nullable();
+            $table->timestamp('last_fetched_at')->nullable();
+            $table->timestamp('last_build_date')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
