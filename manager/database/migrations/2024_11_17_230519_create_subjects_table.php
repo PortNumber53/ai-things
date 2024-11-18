cdd9c91294164e93aee0dc782e506cb7537bdb5a
Mauricio S Otta <mauricio@portnumber53.com>
2024-11-18 01:52:23 -0800

    Added command to identify subjects and keywords, and related model files

---
+++ manager/database/migrations/2024_11_17_230519_create_subjects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->text('keywords')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->integer('podcasts_count')->default(0);
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
