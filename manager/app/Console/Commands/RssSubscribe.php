4441071ef693597f911946e27284aaa50b202eee
Mauricio S Otta <mauricio@portnumber53.com>
2024-11-18 01:55:25 -0800

    Added command to subscribe to RSS feeds

---
+++ manager/app/Console/Commands/RssSubscribe.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RssSubscribe extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Rss:Subscribe';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Subscribe to a RSS feed';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
