f4b7d95e297c8f168bc8c46302937a5b4f924b08
Mauricio S Otta <mauricio@portnumber53.com>
2024-11-25 19:29:43 -0800

    Added fillable for subscriptions

--- manager/app/Models/Subscriptions.php
+++ manager/app/Models/Subscriptions.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Subscriptions extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'feed_url',
+        'title',
+        'description',
+        'site_url',
+        'last_fetched_at',
+        'last_build_date',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'last_fetched_at' => 'datetime',
+        'last_build_date' => 'datetime'
+    ];
 }
