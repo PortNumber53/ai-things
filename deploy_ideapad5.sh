d2ef02d9ac073eb7158c7cd7e168eb4dbcc69164
Mauricio S Otta <mauricio@portnumber53.com>
2024-12-08 22:35:20 -0800

    Enabled SRT service

--- deploy_ideapad5.sh
+++ deploy_ideapad5.sh
@@ -44,6 +44,7 @@ ln -sfn ${RELEASE_FOLDER} ./current
 # Link service file to user's systemd directory
 cd /deploy/ai-things/current
 ln -sfn /deploy/ai-things/current/deploy/systemd/generate_wav.service ~/.config/systemd/user/
+ln -sfn /deploy/ai-things/current/deploy/systemd/generate_srt.service ~/.config/systemd/user/
 
 # Reload user systemd daemon
 systemctl --user daemon-reload
@@ -56,5 +57,17 @@ else
     echo "generate_wav.service not found"
 fi
 
+
+# Check if generate_srt.service exists and restart it if so
+if systemctl --user list-unit-files | grep -q generate_srt.service; then
+    echo "restarting generate_srt.service"
+    systemctl --user restart generate_srt.service
+else
+    echo "generate_srt.service not found"
+fi
+
 # Enable generate_wav.service
 systemctl --user enable generate_wav.service
+
+# Enable generate_srt.service
+systemctl --user enable generate_srt.service
