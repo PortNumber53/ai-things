45b4e5605cb5e17f7629433fc72ea85beb033ea0
Mauricio S Otta <mauricio@portnumber53.com>
2026-05-10 18:54:15 -0700

    Remove Laravel manager folder and rename manager-go to manager (Go version)

--- manager/config/tts.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'onnx_model' => env('TTS_ONNX_MODEL'),
-    'config_file' => env('TT_CONFIG_FILE'),
-    'voice' => env('TTS_VOICE'),
-];
--- manager/db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-psql -h pinky.tail87917.ts.net -p 5432 db_ai_things -U ai_things
